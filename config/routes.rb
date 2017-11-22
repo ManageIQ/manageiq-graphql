563e2cceb30619f7753ebaa7b2089896d140a551
Chris Arcand <chris@chrisarcand.com>
2017-11-22 14:28:27 -0600

    Drop GraphiQL, leave it to the application
    Let the application handle this dependency as graphiql-rails just works
    better when you're doing it outside the engine. Also keeps routes
    concerns in the same spot.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
 ManageIQ::GraphQL::Engine.routes.draw do
   root :to => 'application#execute', :as => :endpoint
-
-  if Rails.env.development?
-    mount GraphiQL::Rails::Engine, :at => '/explorer', :graphql_path => '/graphql'
-  end
 end
