2a84d91aeab8b25c0ed58d4edc1cc787227a21a2
Chris Arcand <chris@chrisarcand.com>
2017-11-22 16:45:40 -0600

    Inflections have to be done *before* namespacing
    Or routing is already set and everything breaks because routes namespace
    to "manage_iq/graph_ql". Ugh.

--- lib/manageiq/graphql/engine.rb
+++ lib/manageiq/graphql/engine.rb
@@ -1,13 +1,13 @@
 module ManageIQ
   module GraphQL
     class Engine < ::Rails::Engine
-      isolate_namespace ManageIQ::GraphQL
-
       ActiveSupport::Inflector.inflections(:en) do |inflect|
         inflect.acronym("ManageIQ")
         inflect.acronym("GraphQL")
         inflect.acronym("GraphiQL")
       end
+
+      isolate_namespace ManageIQ::GraphQL
     end
   end
 end
