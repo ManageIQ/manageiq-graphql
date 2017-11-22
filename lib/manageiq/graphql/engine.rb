8739356f890e9cc0866577941f9c2ba54b316731
Chris Arcand <chris@chrisarcand.com>
2017-11-22 13:32:42 -0600

    Add proper inflections

--- lib/manageiq/graphql/engine.rb
+++ lib/manageiq/graphql/engine.rb
@@ -2,6 +2,12 @@ module ManageIQ
   module GraphQL
     class Engine < ::Rails::Engine
       isolate_namespace ManageIQ::GraphQL
+
+      ActiveSupport::Inflector.inflections(:en) do |inflect|
+        inflect.acronym("ManageIQ")
+        inflect.acronym("GraphQL")
+        inflect.acronym("GraphiQL")
+      end
     end
   end
 end
