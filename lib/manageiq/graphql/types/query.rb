4de31b2a0e4cad51eb92875cdf2080c25c12b550
Chris Arcand <chris@chrisarcand.com>
2017-11-29 14:56:58 -0600

    Add a Service type with Vm associations

--- lib/manageiq/graphql/types/query.rb
+++ lib/manageiq/graphql/types/query.rb
@@ -1,3 +1,4 @@
+require 'manageiq/graphql/types/service'
 require 'manageiq/graphql/types/vm'
 
 module ManageIQ
@@ -5,6 +6,12 @@ module ManageIQ
     module Types
       Query = ::GraphQL::ObjectType.define do
         name 'Query'
+
+        field :services, !types[Types::Service], "List available services" do
+          resolve -> (obj, args, ctx) {
+            ::Service.all
+          }
+        end
 
         field :vms, !types[Types::Vm], "List available virtual machines" do
           resolve -> (obj, args, ctx) {
