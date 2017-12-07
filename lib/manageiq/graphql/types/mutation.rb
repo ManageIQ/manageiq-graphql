741e6f8b097baab3d45d72136ffc476112c6b398
Chris Arcand <chris@chrisarcand.com>
2017-12-06 21:31:57 -0600

    removeTags mutation

--- lib/manageiq/graphql/types/mutation.rb
+++ lib/manageiq/graphql/types/mutation.rb
@@ -4,10 +4,11 @@ module ManageIQ
       Mutation = ::GraphQL::ObjectType.define do
         name 'Mutation'
 
+        # Note: These are incredibly naive ways of doing a mutation.
+        # Mutations should be implemented with object identification conforming
+        # to the Relay specification:
+        # https://facebook.github.io/relay/graphql/objectidentification.htm
         field :addTags, Taggable do
-          # Note: This is an incredibly naive way of doing a mutation.
-          # Mutations should be implemented with object identification
-          # conforming to the Relay specification: https://facebook.github.io/relay/graphql/objectidentification.htm
           description "Adds tags to a Taggable"
           argument :taggableId, !types.ID
           argument :taggableType, !types.String
@@ -18,6 +19,21 @@ module ManageIQ
             klass = "::#{args[:taggableType]}".constantize
             taggable = klass.find(args[:taggableId])
             taggable.tag_add(args[:tagNames])
+            taggable.reload
+          }
+        end
+
+        field :removeTags, Taggable do
+          description "Remove tags from a Taggable"
+          argument :taggableId, !types.ID
+          argument :taggableType, !types.String
+          argument :tagNames, !types[types.String]
+
+          resolve ->(object, args, ctx) {
+            # WARNING: This isn't actually safe, it's merely for PoC
+            klass = "::#{args[:taggableType]}".constantize
+            taggable = klass.find(args[:taggableId])
+            taggable.tag_remove(args[:tagNames])
             taggable.reload
           }
         end
