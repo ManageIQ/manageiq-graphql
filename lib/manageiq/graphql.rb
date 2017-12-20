c1406ae46223bf7c565c589d436da9093afd173a
Chris Arcand <chris@chrisarcand.com>
2017-12-20 10:41:10 -0600

    Add basic VM power operations
    Note that a lot of code here is very PoC:
    
    * Minimal and very generic error handling
    * Formalized a ActionResultPayload, which I basically ported from the
    REST API. I don't really like the format of this but again, not
    important right now.
    * QueueService stuff which again, I basically just ported from the REST
    API and I don't know if I like the API for at all.

--- lib/manageiq/graphql.rb
+++ lib/manageiq/graphql.rb
@@ -3,5 +3,9 @@ require 'graphql/batch'
 require 'graphql/preload'
 
 require 'manageiq/graphql/version'
+
+# TODO Automate/move these internal deps
+require 'manageiq/graphql/queue_service'
+
 require 'manageiq/graphql/engine'
 require 'manageiq/graphql/schema'
--- lib/manageiq/graphql/types/mutation.rb
+++ lib/manageiq/graphql/types/mutation.rb
@@ -1,3 +1,6 @@
+require 'manageiq/graphql/types/vm_power_operation'
+require 'manageiq/graphql/types/action_result_payload'
+
 module ManageIQ
   module GraphQL
     module Types
@@ -35,6 +38,38 @@ module ManageIQ
             taggable = klass.find(args[:taggableId])
             taggable.tag_remove(args[:tagNames])
             taggable.reload
+          }
+        end
+
+        # TODO: This is very PoC
+        field :performVmPowerOperation, ActionResultPayload do
+          name 'performVmPowerOperation'
+          description "Perform power operations on Virtual Machines"
+          argument :vm_id, !types.ID
+          argument :operation, VmPowerOperation
+
+          resolve ->(object, args, ctx) {
+            begin
+              vm = ::Vm.find(args[:vm_id])
+              vm = ::Rbac.filtered_object(vm)
+              description = "Performing #{args[:operation]} operation on VM id: #{vm.id}, name: '#{vm.name}'"
+
+              task_id = QueueService.enqueue(
+                vm,
+                description,
+                :method_name => args[:operation],
+                :role        => "ems_operations"
+              )
+
+              # TODO: ActionResultPayloads probably won't be a thing anyway, but even if they were to be
+              # this should be a proper object mapping to that type, not just OpenStruct....maybe.
+              # TODO: We shouldn't be rescuing all errors like we do in the REST API.
+              # We should be explicitly defining exceptional behavior from core and returning that, but
+              # properly raising our own faults.
+              OpenStruct.new(:success => true, :message => description, :task_id => task_id)
+            rescue StandardError => error
+              OpenStruct.new(:success => false, :message => error.to_s)
+            end
           }
         end
       end
