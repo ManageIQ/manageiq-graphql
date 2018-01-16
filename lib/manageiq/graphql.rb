f5f11e7b938859576a699e59f401911574779728
Chris Arcand <chris@chrisarcand.com>
2018-01-15 22:17:05 -0600

    Use Rails' autoloading/code reloading
    The project now has code reloading :tada:
    
    I originally had kept it PORO with just normal Ruby requires, but we
    really do want live code reloading and you *must* go all-in with Rails'
    dependency stuffs, including autoloading, to make that work.

--- lib/manageiq/graphql.rb
+++ lib/manageiq/graphql.rb
@@ -1,7 +1,6 @@
+require 'graphql'
+require 'graphql/batch'
+require 'graphql/preload'
+
 require 'manageiq/graphql/version'
-
-# TODO Automate/move these internal deps
-require 'manageiq/graphql/queue_service'
-
 require 'manageiq/graphql/engine'
-require 'manageiq/graphql/schema'
--- lib/manageiq/graphql/engine.rb
+++ lib/manageiq/graphql/engine.rb
@@ -13,6 +13,8 @@ module ManageIQ
 
       isolate_namespace ManageIQ::GraphQL
 
+      config.autoload_paths << root.join('lib')
+
       initializer "graphql.add_rest_proxy" do |app|
         app.middleware.use ManageIQ::GraphQL::RESTAPIProxy
       end
