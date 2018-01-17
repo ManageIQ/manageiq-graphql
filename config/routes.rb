79074fba14f140ef9fb11303430ff1fa6782ad38
Tim Wade <hello@timjwade.com>
2018-01-17 12:03:35 -0800

    Mount endpoints engine-side rather than in core
    In response to
    https://github.com/ManageIQ/manageiq/pull/16829#discussion_r161852825.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 ManageIQ::GraphQL::Engine.routes.draw do
   root :to => 'graphql#execute', :as => :endpoint, via: :post
 end
+
+Rails.application.routes.draw do
+  mount ManageIQ::GraphQL::Engine, :at => '/graphql'
+
+  if Rails.env.development?
+    require 'graphiql/rails'
+    mount GraphiQL::Rails::Engine, at: "/graphql/explorer", graphql_path: "/graphql"
+  end
+end
