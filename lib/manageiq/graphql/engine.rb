2fe4ab2cf03e76c57c6c3b5e6748bfb0cd3211b7
Nick LaMuro <nicklamuro@gmail.com>
2018-01-18 13:00:52 -0600

    Add comment about leak in engine.rb

--- lib/manageiq/graphql/engine.rb
+++ lib/manageiq/graphql/engine.rb
@@ -13,6 +13,14 @@ module ManageIQ
 
       isolate_namespace ManageIQ::GraphQL
 
+      # NOTE:  If you are going to make changes to autoload_paths, please make
+      # sure they are all strings.  Rails will push these paths into the
+      # $LOAD_PATH.
+      #
+      # More info can be found in the ruby-lang bug:
+      #
+      #   https://bugs.ruby-lang.org/issues/14372
+      #
       config.autoload_paths << root.join('lib').to_s
 
       initializer "graphql.add_rest_proxy" do |app|
