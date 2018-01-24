a36e51ed699d5410c197b4623ba8d4263fcc4f0d
Tim Wade <hello@timjwade.com>
2018-01-24 14:49:57 -0800

    Support queries in query strings via POST

--- spec/integration/querying_spec.rb
+++ spec/integration/querying_spec.rb
@@ -12,5 +12,31 @@ RSpec.describe "Querying" do
       expect(response).to have_http_status(:ok)
       expect(response.parsed_body).to eq("data" => {"viewer" => {"name" => "Alice"}})
     end
+
+    example "queries can be sent in the query string via the POST method" do
+      query = CGI.escape("{viewer{name}}")
+
+      post(
+        "/graphql?query=#{query}",
+        :headers => {"Authorization" => encode_basic_auth_credentials(user.userid, user.password)},
+      )
+
+      expect(response).to have_http_status(:ok)
+      expect(response.parsed_body).to eq("data" => {"viewer" => {"name" => "Alice"}})
+    end
+
+    it "favors the query string if both a query string and a JSON body are provided" do
+      query = CGI.escape("{viewer{name}}")
+
+      post(
+        "/graphql?query=#{query}",
+        :headers => {"Authorization" => encode_basic_auth_credentials(user.userid, user.password)},
+        :params  => {:query => "{ vms { name } }"},
+        :as      => :json
+      )
+
+      expect(response).to have_http_status(:ok)
+      expect(response.parsed_body).to eq("data" => {"viewer" => {"name" => "Alice"}})
+    end
   end
 end
